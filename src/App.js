f92c0184995060133bc32ae31765440f0fe69372
ranaaezie <ranaaezie@gmail.com>
2022-06-06 22:54:50 -0400

    Added navigation to home-page using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Route, Routes} from 'react-router-dom';
+
+const HatsPage = (props) => {
+  console.log(props);
+  return(
+  <div> 
+    <h1> Hats Page </h1>
+  </div>)
+}
+
+
+const SneakersPage = () => (
+  <div>
+    <h1> Sneakers Page </h1>
+  </div>
+)
+
+
+const MenPage = () => (
+  <div>
+    <h1> Men Page </h1>
+  </div>
+)
+
+
+const WomenPage = () => (
+  <div>
+    <h1> Women Page </h1>
+  </div>
+)
+
+
+const JacketsPage = () => (
+  <div>
+    <h1> Hats Page </h1>
+  </div>
+)
 
 function App() {
   return (
     <div >
-      <HomePage />
+      <Routes>
+        
+      <Route path='/' element={<HomePage />} />
+      <Route path='/shop/hats' element={<HatsPage />} />
+      <Route path='/shop/sneakers' element={<SneakersPage />} />
+      <Route path='/shop/jackets' element={<JacketsPage />} />
+      <Route path='/shop/mens' element={<MenPage />} />
+      <Route path='/shop/womens' element={<WomenPage />} />
+      
+
+      </Routes>
+     
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -50,8 +50,8 @@ class  Directory extends React.Component {
         return (
             <div className='directory-menu'>
                 {
-                    this.state.sections.map( ({title , imageUrl , id}) => (
-                        <MenuItem key={id} title={title}  imageUrl={imageUrl} />
+                    this.state.sections.map( ({title , imageUrl , id, linkUrl}) => (
+                        <MenuItem key={id} title={title}  imageUrl={imageUrl} url={linkUrl} />
                     ) )
                 }
             </div>
